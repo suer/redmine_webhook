9203370fff87e8e2c798d2c6c95b5b91deb56381
Ryo SUETSUGU <suetsugu.r@gmail.com>
2019-05-02 10:28:53 +0900

    Bumped: 0.0.4

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_webhook do
   name 'Redmine Webhook plugin'
   author 'suer'
   description 'A Redmine plugin posts webhook on creating and updating tickets'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/suer/redmine_webhook'
   author_url 'http://d.hatena.ne.jp/suer'
   permission :manage_hook, {:webhook_settings => [:show,:update,:create, :destroy]}, :require => :member
