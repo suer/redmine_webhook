9acb46708aba8585cbb7a23467449a8d6e5db815
Ryo SUETSUGU <suetsugu.r@gmail.com>
2019-05-25 20:49:04 +0900

    Added: specify rails release version for migration

--- db/migrate/001_create_webhooks.rb
+++ db/migrate/001_create_webhooks.rb
@@ -1,8 +1,8 @@
-class CreateWebhooks < ActiveRecord::Migration
+class CreateWebhooks < ActiveRecord::Migration[4.2]
   def change
     create_table :webhooks do |t|
       t.string :url
-      t.integer :project_id 
+      t.integer :project_id
     end
   end
 end
